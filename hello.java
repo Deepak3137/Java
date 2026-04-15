760779b0989a94832c9051522990f38c47800f86
Deepak Kumar <deepakkumar813369@gmail.com>
2026-04-15 23:18:30 +0530

    Learning about the classes

--- hello.java
+++ hello.java
@@ -1,7 +1,24 @@
-class hello {
-  public static void main(String a[]) {
-    System.out.println("Hello World");
-    System.out.println("hello");
+class world {
+  int nums[] = new int[4];
+  public void assign(){
+    for(int i=0; i<4; i++) {
+      nums[i] = i;
+    }
+  }
 
+  public void print() {
+    System.out.println("Inside the print function");
+    for(int i=0; i<4; i++) {
+      System.out.println(nums[i]);
+    }
+  }
+}
+
+class hello {
+  public static void main(String arg[]) {
+    world obj = new world();
+
+    obj.assign();
+    obj.print();
   }
 }
