98a35ed61dbc7b80477398b3bec9a294e860fe24
Deepak Kumar <deepakkumar813369@gmail.com>
2026-04-15 23:26:57 +0530

    multidimentionalArray

---
+++ multidimentionalArray.java
@@ -0,0 +1,18 @@
+class multidimentionalArray {
+  public static void main() {
+    int arr [][] = new int [2][3];
+
+    for(int i=0; i<2; i++) {
+      for(int j=0; j<3; j++) {
+        arr[i][j] = (int)(Math.random() * 10);
+      }
+    }
+
+    for(int i=0; i<2; i++) {
+      for(int j=0; j<3; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
