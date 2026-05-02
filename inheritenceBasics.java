58fba590c51c1faff0a9167624fd04cc2c286f5b
Deepak Kumar <deepakkumar813369@gmail.com>
2026-05-03 00:51:06 +0530

    inheritence basics

---
+++ inheritenceBasics.java
@@ -0,0 +1,27 @@
+public class inheritenceBasics {
+  public static void main(String[] args) {
+    // in inheritence we can use the properties of one class in another class.
+    // this is done by using the keyword extends.
+    // the class which is inherited is called parent class or super class.
+    // the class which inherits is called child class or sub class.
+
+    // example
+    // parent class
+    class animal {
+      void eat() {
+        System.out.println("eating...");
+      }
+    }
+
+    // child class
+    class dog extends animal {
+      void bark() {
+        System.out.println("barking...");
+      }
+    }
+
+    dog d = new dog();
+    d.eat(); // eating...
+    d.bark(); // barking...
+  }
+}
