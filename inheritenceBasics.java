6a916fe26feb81756a8458f6a40563fe9cd693fb
Deepak Kumar <deepakkumar813369@gmail.com>
2026-05-03 00:55:39 +0530

    inheritence basics single&multilevel

--- inheritenceBasics.java
+++ inheritenceBasics.java
@@ -23,5 +23,8 @@ public class inheritenceBasics {
     dog d = new dog();
     d.eat(); // eating...
     d.bark(); // barking...
+
+
+    //this is single level inheritence, we can also extends animal class in another class and then that class can be extended by another class and so on. this is called multi level inheritence.
   }
 }
