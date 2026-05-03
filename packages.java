7ec4fdc3f0e65053e12fc094af3f9f613fc5c0b7
Deepak Kumar <deepakkumar813369@gmail.com>
2026-05-03 18:07:55 +0530

    packages

---
+++ packages.java
@@ -0,0 +1,26 @@
+public class packages {
+  public static void main(String[] args) {
+    // packages are used to group related classes and interfaces together. it is used to avoid name conflicts and to control access to the classes and interfaces.
+
+    // example
+    // we can create a package named com.example and then we can create a class named student in that package.
+
+    // package com.example;
+    // public class student {
+    //   void display() {
+    //     System.out.println("hello from student class");
+    //   }
+    // }
+
+    // now we can use this class in another class by importing the package.
+
+    // import com.example.student;
+    // public class test {
+    //   public static void main(String[] args) {
+    //     student s = new student();
+    //     s.display(); // hello from student class
+    //   }
+    // }
+
+  }
+}
