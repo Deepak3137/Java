d3648b66b3672dc30eb72915fdd8d6e37df903bc
Deepak Kumar <deepakkumar813369@gmail.com>
2026-05-11 22:52:59 +0530

    adding the basics

--- interfaceBasics.java
+++ interfaceBasics.java
@@ -7,6 +7,19 @@ interface A {
   // bacause interface is a collection of abstract methods and static constants.
   // so we can't change the value of myVar in the implementation class.
 
+  // static in interface means that the variable belongs to the interface and not
+  // to any instance of the interface. it can be accessed using the interface
+  // name, like A.myVar.
+
+  // we can also refrence the variable using the instance of the interface, but it
+  // is not recommended because it can lead to confusion and it is not clear that
+  // the variable belongs to the interface and not to the instance of the
+  // interface. for example:
+  // A obj = new MyClass();
+  // System.out.println(obj.myVar); // this is correct but it is not recommended
+  // because it can lead to confusion and it is not clear that the variable
+  // belongs to the interface and not to the instance of the interface.
+
   void myMethod();
   // public abstract void myMethod(); this is also correct since all the methods
   // in the interface are abstract and public by default.
@@ -15,6 +28,7 @@ interface A {
 // we can implement an interface using the implements keyword. the syntax for
 // implementing an interface is as follows
 class MyClass implements A {
+  int var = 20;
   public void myMethod() {
     System.out.println("Hello from MyClass");
   }
@@ -43,6 +57,18 @@ public class interfaceBasics {
 
     MyClass obj = new MyClass();
     obj.myMethod(); // Hello from MyClass
+
+    // A a; // we can also declare a reference variable of the interface type and assign it
+       // to an instance of the class that implements the interface. this is called
+       // upcasting. upcasting is the process of converting a subclass type to a
+       // superclass type. it is done implicitly by the java compiler. it is also
+       // known as widening conversion.
+    System.out.println(A.myVar); // 10 - we can access the static variable of the interface using the interface
+                                 // name
+    // System.out.println(a.myVar); // 10 - we can also access the static variable
+    // of the interface using the instance of the interface, but it is not
+    // recommended because it can lead to confusion and it is not clear that the
+    // variable belongs to the interface and not to the instance of the interface.
 
   }
 }
--- upcastingAndDowncasting.java
+++ upcastingAndDowncasting.java
@@ -1,6 +1,7 @@
 public class upcastingAndDowncasting {
   public static void main(String[] args) {
-    // upcasting is the process of converting a subclass type to a superclass type. it
+    // upcasting is the process of converting a subclass type to a superclass type.
+    // it
     // is done implicitly by the java compiler. it is also known as widening
     // conversion. downcasting is the process of converting a superclass type to a
     // subclass type. it is done explicitly by the programmer. it is also known as
@@ -28,5 +29,17 @@ public class upcastingAndDowncasting {
     // a.isPet(); // error: cannot find symbol method isPet() in animal class
 
     ((dog) a).isPet(); // downcasting, true
+
+    //dog d = new animal(); // error: incompatible types: animal cannot be converted to dog
+    // to avoid this error, we need to use downcasting
+   // dog d = (dog) new animal(); // downcasting, but it will throw a ClassCastException at runtime because the
+                                // actual object is of type animal and cannot be cast to type dog
+
+    // to avoid this error, we can use the instanceof operator to check if the object is of the correct type before downcasting
+    if (a instanceof dog) {
+      dog d1 = (dog) a; // downcasting, but it will not throw a ClassCastException at runtime because the actual object is of type dog and can be cast to type dog
+      System.out.println(d1.isPet()); // true
+    } else {
+      System.out.println("The object is not of type dog");  
   }
 }
